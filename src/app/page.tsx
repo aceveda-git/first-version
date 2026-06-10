019d188a9649ce8b7ff9777fb3eb11836edfa30e
aceveda-git <aceveda@effectussoftware.com>
2026-06-10 17:23:15 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,9 @@ export default function HomePage() {
         <p className="page__eyebrow">Effectus</p>
         <h1 className="page__title">World Cup 2026</h1>
         <p className="page__subtitle">
-          Open your personal link to reveal your sticker pack. Add /name-surname to url.
+          Open your personal link to reveal your sticker pack.
+          <br />
+          Add /name-surname to the URL.
         </p>
       </header>
     </main>
